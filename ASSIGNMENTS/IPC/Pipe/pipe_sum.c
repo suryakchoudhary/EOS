2a49c10c7c9022e9097640e708079d239bfc13cc
suryakchoudhary <surya333kant@gmail.com>
2021-12-03 15:02:35 +0530

    Assignment Day- 6 Updated

--- ASSIGNMENTS/IPC/Pipe/pipe_sum.c
+++ ASSIGNMENTS/IPC/Pipe/pipe_sum.c
@@ -5,48 +5,38 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
+
 int main()
 {
-	int R1, R2, W1, W2, sumW=0, sumR=0;
+	int sum=0;
 	int id , pip_fd[2];
 	pipe(pip_fd);
 	id = fork();
+	int arr[2];
 
 	if(id==0)
 	{
-		//Reading 1st integer
-		read(pip_fd[0],&R1 , sizeof(int));
-		printf("%d  1st int is received at child end.\n",R1);
-
-		//Reading 2nd integer
-		read(pip_fd[0],&R2 , sizeof(int));
-                printf("%d  2nd int is received at child end.\n",R2);
-		close(pip_fd[0]);
+		//Reading two integers
+		read(pip_fd[0],arr , sizeof(arr));
+		printf("%d and %d  integers are received at child end.\n",arr[0], arr[1]);
 
 		//Calculating sum
-		sumW=R1+R2;
-		write(pip_fd[1], &sumW, sizeof(int));
+		sum=arr[0] + arr[1];
+		write(pip_fd[1], &sum, sizeof(int));
 		close(pip_fd[1]);
 	}
 	else
 	{
-		//Writing 1st integer
-		printf("Enter first integers for sending it to child.\n");
-		scanf("%d",&W1);
-		write(pip_fd[1],&W1,sizeof(int));
-
-		//Writing 1st integer
-		sleep(0.5);
-		printf("Enter 2nd integers for sending it to child.\n");
-                scanf("%d",&W2);
-                write(pip_fd[1],&W2,sizeof(int));
-		close(pip_fd[1]);
+		//Writing two integers
+		printf("Enter two integers for sending it to child.\n");
+		scanf("%d %d",&arr[0], &arr[1]);
+		write(pip_fd[1],&arr,sizeof(arr));
 
 		//Reading sum
-		sleep(0.5);
-		read(pip_fd[0], &sumR, sizeof(int));
-		printf("sum = %d\n", sumR);
+		sleep(0.001);
+		read(pip_fd[0], &sum, sizeof(int));
+		printf("sum = %d\n", sum);
 		close(pip_fd[0]);	
 	}
 	return 0;
-}
+}     
