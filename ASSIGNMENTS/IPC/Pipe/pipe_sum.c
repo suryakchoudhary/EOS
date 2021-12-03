76c9a86ad8122e7ee7141f9e3f32a3d574bbe8a3
suryakchoudhary <surya333kant@gmail.com>
2021-12-03 17:04:26 +0530

    Assignment Day- 6

--- ASSIGNMENTS/IPC/Pipe/pipe_sum.c
+++ ASSIGNMENTS/IPC/Pipe/pipe_sum.c
@@ -1,6 +1,5 @@
 /*Write a program such that parest passes two integers to the child process and then child receives it 
   and sum it and again send its sum to the parent process and parent receives the process and displays it */
-  // USING SINGLE PIPE
 
 #include <stdio.h>
 #include <sys/types.h>
@@ -9,34 +8,44 @@
 int main()
 {
 	int sum=0;
-	int id , pip_fd[2];
+	int id , pip_fd[2], pip_fd1[2];
 	pipe(pip_fd);
-	id = fork();
+	pipe(pip_fd1);
 	int arr[2];
+	id = fork();
 
 	if(id==0)
 	{
+		//Pipe 1 
 		//Reading two integers
+		close(pip_fd[1]);
 		read(pip_fd[0],arr , sizeof(arr));
 		printf("%d and %d  integers are received at child end.\n",arr[0], arr[1]);
+		close(pip_fd[0]);
 
+		//Pipe 2
 		//Calculating sum
+		close(pip_fd1[0]);
 		sum=arr[0] + arr[1];
-		write(pip_fd[1], &sum, sizeof(int));
-		close(pip_fd[1]);
+		write(pip_fd1[1], &sum, sizeof(int));
+		close(pip_fd1[1]);
 	}
 	else
 	{
+		//Pipe 1
 		//Writing two integers
+		close(pip_fd[0]);
 		printf("Enter two integers for sending it to child.\n");
 		scanf("%d %d",&arr[0], &arr[1]);
 		write(pip_fd[1],&arr,sizeof(arr));
+		close(pip_fd[1]);
 
+		//Pipe 2
 		//Reading sum
-		sleep(0.001);
-		read(pip_fd[0], &sum, sizeof(int));
+		close(pip_fd1[1]);
+		read(pip_fd1[0], &sum, sizeof(int));
 		printf("sum = %d\n", sum);
-		close(pip_fd[0]);	
+		close(pip_fd1[0]);	
 	}
 	return 0;
 }     
