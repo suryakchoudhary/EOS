aff359ba2788829f11533eb125f5645599b0f2dc
suryakchoudhary <surya333kant@gmail.com>
2021-12-03 23:58:03 +0530

    Read Writing Through FIFO (Assignment Day- 6)

--- ASSIGNMENTS/IPC/FIFO/pro_2.c
+++ ASSIGNMENTS/IPC/FIFO/pro_2.c
@@ -7,12 +7,16 @@
 
 int main()
 {
-	int fd;
+	int fd, fd1;
 	char buff[100];
 	fd = open("spec_fifo",O_RDONLY);
 	read(fd, buff, 100);
-	printf("READ: %s\n", buff);
+	printf("READ from p1: %s\n", buff);
 	close(fd);
+
+	fd1 = open("spec_fifo", O_WRONLY);
+	write(fd,"Writing this in process 2 and it will be read by process 1.\n",sizeof("    Writing this in process 2 and it will be read by process 1.\n"));
+	close(fd1);
 	return 0;
 }
 
